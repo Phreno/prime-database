d666bfcf8b964ae5708f812b58ffbbd327d178ef
K3rn€lP4n1k <losgoomy@gmail.com>
2017-04-15 00:54:03 +0200

    ajout cartouche

--- sample/twins.sql
+++ sample/twins.sql
@@ -1,5 +1,29 @@
+-- 
+-- PrimeDB - Sélection des nombres premiers jumeaux
+--
+
+-- ======================
+-- INFORMATIONS GÉNÉRALES
+-- ======================
+
+-----------------------
+-- MODIFIE LA BASE: NON 
+-----------------------
+
+-- Date........ samedi 15 avril 2017, 00:49:54 (UTC+0200)
+-- Objectifs... Récupérer la suite des nombres premiers jumeaux
+-- Intention... Créer un premier fichier d'exemple
+-- Etat........ TESTED
+
+------------------
+-- Commentaires --
+------------------
+-- N/A
+
 .separator ;
-select p1.value, p2.value 
-from prime as p1
-inner join prime as p2 on p1.value = p2.value - 2
+SELECT 	  p1.value
+	, p2.value
+ 
+FROM 	prime AS p1
+	INNER JOIN prime AS p2 ON p1.value = p2.value - 2
 ;
